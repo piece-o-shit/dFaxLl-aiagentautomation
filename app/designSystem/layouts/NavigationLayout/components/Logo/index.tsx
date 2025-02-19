008a627118757130cdf537bb75f8d2ff18b77f27
Marblism CLI <studio@marblism.com>
2025-02-19 22:00:04 +0000

    Session saved

--- app/designSystem/layouts/NavigationLayout/components/Logo/index.tsx
+++ app/designSystem/layouts/NavigationLayout/components/Logo/index.tsx
@@ -21,7 +21,6 @@ export const Logo: React.FC<Props> = ({
   return (
     <Flex align="center" gap={10} onClick={() => goTo('/home')}>
       <img
-        src="https://marblism-dashboard-api--production-public.s3.us-west-1.amazonaws.com/dFaxLl-aiagentautomation-NMrK"
         {...props}
         alt="Logo"
         height={height}
